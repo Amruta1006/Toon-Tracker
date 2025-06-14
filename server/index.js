f08bc4401bb86d25357dea1638e7cea333030481
Amruta <amrutabmaskar@gmail.com>
2025-06-14 14:38:03 +0530

    added basic get and post method and check it's working

--- server/index.js
+++ server/index.js
@@ -5,9 +5,39 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const TV_SHOWS = []
+
 //health api to check if the server is running
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'Server is running' });
+});
+
+// API to get all TV shows or read all shows
+app.get('/tv-shows', (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: TV_SHOWS,
+        message: 'TV shows fetched successfully'
+    });
+}); 
+
+// API to add a new TV show
+app.post('/tv-shows', (req, res) => {
+    const {title,time,channel,thumbnail} = req.body;
+    //object of tv shows
+    const newShow = {
+        title,
+        time,
+        channel,
+        thumbnail
+    };
+    TV_SHOWS.push(newShow);
+
+    return res.status(201).json({
+        success: true,
+        data: newShow,
+        message: 'TV show added successfully'
+    });
 });
 
 const PORT = 5002;
